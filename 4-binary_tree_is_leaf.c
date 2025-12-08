64b967623e810a44a98126fd0074144ae1b36a5b
MoyeAnthonia <moye.anthonia@gmail.com>
2025-12-08 13:27:50 +0000

    if leaf exist

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf -  function that checks if
+ * a node is a leaf
+ * @node: pointer to the node
+ * Return: pointer
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (!node)
+return (0);
+
+if (!node->left && !node->right)
+return (1);
+
+return (0);
+}
