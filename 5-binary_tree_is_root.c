cbd5a97ab7cc61ab825283844faeaf3efa7aa274
MoyeAnthonia <moye.anthonia@gmail.com>
2025-12-08 13:41:05 +0000

    more cpmmands

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root -  function that checks if
+ * a node is a root
+ * @node: pointer to the node
+ * Return: pointer
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (!node)
+return (0);
+
+if (!node->parent)
+return (1);
+
+return (0);
+}
